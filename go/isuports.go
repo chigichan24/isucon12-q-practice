d841282b58d517d0d2e07f774f95628283cdcfcc
Kazuki Chigita <kazuki.chigichan@gmail.com>
2023-09-02 01:58:33 +0900

    Fix type of player id

--- go/isuports.go
+++ go/isuports.go
@@ -431,7 +431,7 @@ type PlayerScoreAndPlayerRow struct {
 	CreatedAt     int64  `db:"player_score.created_at"`
 	UpdatedAt     int64  `db:"player_score.updated_at"`
 	DisplayName   string `db:"player.display_name"`
-	PlayerRawID   int64  `db:"player.id"`
+	PlayerRawID   string `db:"player.id"`
 }
 
 // 排他ロックのためのファイル名を生成する
